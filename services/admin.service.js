c49ce4bf89c7eecb9b5cba5116c223b3745ae897
jaddielorzano-pointwest <jaddie.lorzano@pointwest.com.ph>
2022-10-13 06:05:17 +0800

    CREATE admin service

--- services/admin.service.js
+++ services/admin.service.js
@@ -1,1 +1,23 @@
-export default AdminService
+export default class AdminService {
+    constructor({
+
+    }) {
+        
+    }
+
+    // * GET /admin/reimbursements?cut-off
+    // US009: As an HR, I can view all reimbursements submitted in a cut-off
+
+    // * GET /admin/reimbursements/:reimbursementId
+    // US0010: As an HR, I can view the details of a reimbursement
+
+    // * GET /admin/reimbursements/search?byEmployeeNumber&byLastName&byFirstName
+    // US0011: As an HR, I can search for a particular reimbursement submitted by an employee
+
+    // * POST /admin/reimbursements/:reimbursementId/approve
+    // US0012: As an HR, I can approve submitted reimbursements
+
+    // * POST /admin/reimbursements/:reimbursementId/reject
+    // US0013: As an HR, I can reject submitted reimbursements
+
+}
