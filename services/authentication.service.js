f0927e8f74ee9df969db13b9cb12a593f96f05d9
jaddielorzano-pointwest <jaddie.lorzano@pointwest.com.ph>
2022-10-13 06:05:34 +0800

    CREATE authentication service

---
+++ services/authentication.service.js
@@ -0,0 +1,15 @@
+export default class AuthenticationService {
+    constructor({
+
+    }) {
+
+    }
+    
+    // * POST /logout
+
+    // * All APIs after login should be secured and only accessible to logged in user.
+    authenticateUser() {
+        // authenticate user
+    }
+
+}
